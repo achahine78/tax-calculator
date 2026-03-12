5696e0fcba67894f7cf49fdad0e2131d95bb1001
achahine78 <achahine78@gmail.com>
2026-03-12 17:46:01 -0400

    feat: add basic styles to tax table

--- src/components/TaxTable.tsx
+++ src/components/TaxTable.tsx
@@ -1,22 +1,44 @@
 import type { TaxCalculation } from "../utils/taxCalculator";
 
+import "./TaxTable.css";
+
 type Props = {
     taxes: TaxCalculation;
 };
 
 export const TaxTable = ({ taxes }: Props) => {
     return (
-        <div>
-            <div>Total Taxes: {taxes.totalTax}</div>
-            {taxes.taxesPerBracket.map(({ max, min, rate, taxOwed }) => (
-                <div>
-                    <div>Max: {max}</div>
-                    <div>Min: {min}</div>
-                    <div>Rate: {rate}</div>
-                    <div>Taxes Owed: {taxOwed}</div>
+        <div className="tax-table-container">
+            <div className="tax-table-header">
+                <div className="tax-table-header__column">
+                    <div>Gross Income</div>
+                    <div>{taxes.annualIncome}</div>
                 </div>
-            ))}
-            <div>Effective Rate: {taxes.effectiveRate}</div>
+                <div className="tax-table-header__column">
+                    <div>Total Tax</div>
+                    <div>{taxes.totalTax}</div>
+                </div>
+                <div className="tax-table-header__column">
+                    <div>Effective Rate</div>
+                    <div>{taxes.effectiveRate}</div>
+                </div>
+            </div>
+            <div className="tax-table">
+                <div className="tax-table__row">
+                    <div className="tax-table__column">Tax Bracket</div>
+                    <div className="tax-table__column">Rate</div>
+                    <div className="tax-table__column">Tax Owed</div>
+                </div>
+                {taxes.taxesPerBracket.map(({ max, min, rate, taxOwed }) => (
+                    <div className="tax-table__row">
+                        <div className="tax-table__column">
+                            {min}-{max}
+                        </div>
+                        <div className="tax-table__column">{rate}</div>
+                        <div className="tax-table__column">{taxOwed}</div>
+                    </div>
+                ))}
+            </div>
         </div>
     );
 };
